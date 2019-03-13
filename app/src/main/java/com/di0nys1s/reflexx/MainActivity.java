77185799a6184e6209591b2e978553829e21dcd2
Burak Seyhan <seyhanburak9@gmail.com>
2019-03-13 07:45:20 -0400

    Initial commit

---
+++ app/src/main/java/com/di0nys1s/reflexx/MainActivity.java
@@ -0,0 +1,13 @@
+package com.di0nys1s.reflexx;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
